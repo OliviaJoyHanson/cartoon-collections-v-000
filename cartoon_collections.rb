5f46635e868890879218153db48acd4f56a86091
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-04 14:02:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   end
 end
 
-def long_planeteer_calls(calls)# code an argument here
+def long_planeteer_calls(short_words)# code an argument here
   # Your code here
+
 end
 
 def find_the_cheese(food)# code an argument here

ee03b49b566713e95452526cf717c18c6207938a
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-04 13:53:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,20 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |name, index|
-    puts "#{index + 1} #{name}"
-  end
+
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect do |call|
     #"#{call.capitalize}!"
-    call.capitalize << "!"
-  end
+
 end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.detect do |cheese|
-    food.include?(cheese) 
-  end
+
 end
